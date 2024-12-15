7fd6667f427188b20de43c237e852f1e1b5ba42d
Chirayu Ajay Sahu <81841434+ChirayuSahu@users.noreply.github.com>
2024-12-15 19:26:39 +0530

    Update businessNameGenerator.js

--- businessNameGenerator.js
+++ businessNameGenerator.js
@@ -18,14 +18,40 @@ let obj3={
 
 random=Math.random()
 
+//generating the first word
 if(random<0.33){
-    businessName=obj1[1]+obj2[1]+obj3[1]
+    first=obj1[1]
 }
 else if(0.33<random<0.66){
-    businessName=obj1[2]+obj2[2]+obj3[2]
+    first=obj1[2]
 }
 else{
-    businessName=obj1[3]+obj2[3]+obj3[3]
+    first=obj1[3]
 }
 
+//generating the second word
+if(random<0.33){
+    second=obj2[1]
+}
+else if(0.33<random<0.66){
+    second=obj2[2]
+}
+else{
+    second=obj2[3]
+}
+
+//generating the third word
+if(random<0.33){
+    third=obj3[1]
+}
+else if(0.33<random<0.66){
+    third=obj3[2]
+}
+else{
+    third=obj3[3]
+}
+
+let businessName=(`${first} ${second} ${third}`)
+
+//return name
 console.log(businessName)
